b3fb66110927432678542478cc037c352d6bc6e1
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-01-13 12:37:37 -0500

    tweaks: course status table, lists for the nav sidebars

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -22,7 +22,6 @@ export default function Courses() {
               <Route path="Modules" element={<Modules />} />
               <Route path="Assignments" element={<Assignments />} />
               <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-              <Route path="People" element={<h3>People</h3>} />
             </Routes>
           </td>
         </tr>
--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -2,12 +2,28 @@ import { Link } from "react-router-dom";
 export default function KambazNavigation() {
   return (
     <div id="wd-kambaz-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
-      <Link to="/Kambaz/Account" id="wd-account-link">Account</Link><br/>
-      <Link to="/Kambaz/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link to="/Kambaz/Dashboard" id="wd-course-link">Courses</Link><br/>
-      <Link to="/Kambaz/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link to="/Kambaz/Inbox" id="wd-inbox-link">Inbox</Link><br/>
-      <Link to="/Labs" id="wd-labs-link">Labs</Link><br/>
+        <ul id="wd-kambaz-navigation-list">
+            <li className="wd-kambaz-navigation-list-item">
+                <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
+            </li>
+            <li className="wd-kambaz-navigation-list-item">
+                <Link to="/Kambaz/Account" id="wd-account-link">Account</Link><br/>
+            </li>
+            <li className="wd-kambaz-navigation-list-item">
+                <Link to="/Kambaz/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
+            </li>
+            <li className="wd-kambaz-navigation-list-item">
+                <Link to="/Kambaz/Dashboard" id="wd-course-link">Courses</Link><br/>
+            </li>
+            <li className="wd-kambaz-navigation-list-item">
+                <Link to="/Kambaz/Calendar" id="wd-calendar-link">Calendar</Link><br/>
+            </li>
+            <li className="wd-kambaz-navigation-list-item">
+                <Link to="/Kambaz/Inbox" id="wd-inbox-link">Inbox</Link><br/>
+            </li>
+            <li className="wd-kambaz-navigation-list-item">
+                <Link to="/Labs" id="wd-labs-link">Labs</Link><br/>
+            </li>
+      </ul>
     </div>
 );}
