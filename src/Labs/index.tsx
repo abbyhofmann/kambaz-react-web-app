e7c0441587a8f567088255387dfef51bf8ba652a
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-01-12 20:42:20 -0500

    name change to abigail

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,7 +6,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div>
-      <h1>Abby Hofmann - online section, CRN:  37031</h1>
+      <h1>Abigail Hofmann - online section, CRN:  37031</h1>
       <h1>Labs</h1>
       <TOC />
       <Routes>
