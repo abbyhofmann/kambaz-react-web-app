781af5f5b66603bb4d1b2cd094f52fd63246c40a
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-01-24 14:40:06 -0500

    account screens done

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -1,19 +1,43 @@
+import { Form, FormSelect } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export default function Profile() {
   return (
     <div id="wd-profile-screen">
-      <h3>Profile</h3>
-      <input defaultValue="alice" placeholder="username" className="wd-username"/><br/>
-      <input defaultValue="123"   placeholder="password" type="password"
-             className="wd-password" /><br/>
-      <input defaultValue="Alice" placeholder="First Name" id="wd-firstname" /><br/>
-      <input defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" /><br/>
-      <input defaultValue="2000-01-01" type="date" id="wd-dob" /><br/>
-      <input defaultValue="alice@wonderland" type="email" id="wd-email" /><br/>
-      <select defaultValue="FACULTY" id="wd-role">
-        <option value="USER">User</option>       <option value="ADMIN">Admin</option>
-        <option value="FACULTY">Faculty</option> <option value="STUDENT">Student</option>
-      </select><br/>
-      <Link to="/Kambaz/Account/Signin" >Sign out</Link>
+      <h1>Profile</h1>
+      <Form.Control id="wd-username"
+             placeholder="username"
+             className="mb-2"
+             defaultValue={"alice"}/>
+      <Form.Control id="wd-password"
+             placeholder="password" type="password"
+             className="mb-2"
+             defaultValue={123}/>
+      <Form.Control id="wd-firstname"
+             placeholder="First Name"
+             className="mb-2"
+             defaultValue={"Alice"}/>
+      <Form.Control id="wd-lastname"
+             placeholder="Last Name"
+             className="mb-2"
+             defaultValue={"Wonderland"}/>
+      <Form.Control id="wd-dob"
+             type="date"
+             className="mb-2"
+             defaultValue={"2000-01-01"}/>
+      <Form.Control id="wd-email"
+             type="email"
+             className="mb-2"
+             defaultValue={"alice@wonderland"}/>
+      <FormSelect id="wd-role" defaultValue="FACULTY" className="mb-2">
+        <option selected value="USER">User</option>
+        <option value="FACULTY">Faculty</option>
+        <option value="ADMIN">Admin</option>
+        <option value="STUDENT">Student</option>
+      </FormSelect>
+      <Link id="wd-signout-btn"
+            to="/Kambaz/Account/Signin"
+            className="btn btn-danger w-100 mb-2">
+            Signout 
+      </Link>
     </div>
 );}
