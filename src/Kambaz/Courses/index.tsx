ecb31f915ac20577ea8bba5966c8f23ed504278c
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-01-29 14:31:54 -0500

    courses navbar dropdown updates

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -10,11 +10,10 @@ import KambazOffcanvasNavigation from "../KambazOffcanvasNavigation";
 import { SiCanvas } from "react-icons/si";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import CoursesDropdown from "./CoursesDropdown";
-
 export default function Courses() {
     return (
         <div id="wd-courses">
-            <h2 className="text-white bg-dark d-md-none d-flex align-items-center justify-content-between">
+            <nav className="navbar text-white bg-dark d-md-none d-flex align-items-center justify-content-between">
                 <FaAlignJustify
                     className="me-4 fs-4 mb-1 ms-1 float-start"
                     data-bs-toggle="offcanvas"
@@ -25,21 +24,21 @@ export default function Courses() {
                     <p className="text-uppercase fs-4 mt-2 mb-0">Course 1234 </p>
                     <p className="text-uppercase fs-6 mb-2">modules </p>
                 </div>
-                <div className="dropdown d-md-none float-end">
-                    <RiArrowDropDownLine
-                        className="text-white mb-1 fs-1"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    />
-                    <CoursesDropdown />
-                </div>
-            </h2>
-            <h2 className="text-danger d-none d-md-block">
+                <RiArrowDropDownLine
+                    className="text-white mb-1 fs-1"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation"
+                />
+            </nav>
+            {/* <CoursesDropdown /> */}
+            <CoursesDropdown />
+
+            <nav className="navbar text-danger d-none d-md-block">
                 <FaAlignJustify
                     className="me-4 fs-4 mb-1"
                 />
                 Course 1234
-            </h2>
+            </nav>
             <hr />
 
             {/* the offcanvas nav page is only available by clicking the FaAlignJustify button when the screen is minimized */}
