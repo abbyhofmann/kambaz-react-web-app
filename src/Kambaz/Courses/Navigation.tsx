fe5f5527ad42d46da232c52e5cacf3b15eec084e
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-02-04 16:59:06 -0500

    3.7.5 done

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,20 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useParams, useLocation } from "react-router-dom";
+
 export default function CourseNavigation() {
+  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+  const { cid } = useParams();
+  const { pathname } = useLocation(); 
+
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to="/Kambaz/Courses/1234/Home" id="wd-course-home-link"
-        className="list-group-item active border border-0"> Home </Link>
-      <Link to="/Kambaz/Courses/1234/Modules" id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"> Modules </Link>
-      <Link to="/Kambaz/Courses/1234/Piazza" id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"> Piazza </Link>
-      <Link to="/Kambaz/Courses/1234/Zoom" id="wd-course-zoom-link"
-        className="list-group-item text-danger border border-0"> Zoom </Link>
-      <Link to="/Kambaz/Courses/1234/Assignments" id="wd-course-quizzes-link"
-        className="list-group-item text-danger border border-0"> Assignments </Link>
-      <Link to="/Kambaz/Courses/1234/Quizzes" id="wd-course-assignments-link"
-        className="list-group-item text-danger border border-0"> Quizzes </Link>
-      <Link to="/Kambaz/Courses/1234/People" id="wd-course-people-link"
-        className="list-group-item text-danger border border-0" > People </Link>
+      {links.map((link) => (
+        <Link key={link} to={`/Kambaz/Courses/${cid}/${link}`} className={`list-group-item border border-0
+              ${pathname.includes(link) ? "text-black bg-white active" : "text-danger"}`}>
+          {link}
+        </Link>
+      ))}
     </div>
 );}
