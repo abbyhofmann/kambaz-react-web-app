9165f575652b9c2eb3832e4e8485dc5d85142e88
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-02-10 22:03:23 -0500

    account signin signup profile navigation update

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
 export default function AccountNavigation() {
-  return (
-    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-        <Link to="/Kambaz/Account/Signin" id="wd-account-signin-link"
-            className="list-group-item active border border-0"> Signin </Link>
-        <Link to="/Kambaz/Account/Signup" id="wd-account-signup-link"
-            className="list-group-item text-danger border border-0"> Signup </Link>
-        <Link to="/Kambaz/Account/Profile" id="wd-account-profile-link"
-            className="list-group-item text-danger border border-0"> Profile </Link>
-    </div>
-);}
+    const { pathname } = useLocation();
+    const links = ["Signin", "Signup", "Profile"];
+
+    return (
+        <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+            {links.map((link) => (
+                <Link to={`/Kambaz/Account/${link}`} id={`wd-account-${link}-link`}
+                    className={`list-group-item border border-0 ${pathname.includes(link) ? `active` : `text-danger`}`}> {link} </Link>
+            ))}
+        </div>
+    );
+}
