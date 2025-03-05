bc249a6b03b6de9001c96366bf13f55a7935f625
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-03-04 19:19:11 -0500

    4.4.1.3 wip

--- src/Kambaz/Dashboard/index.tsx
+++ src/Kambaz/Dashboard/index.tsx
@@ -1,12 +1,36 @@
 import { Link } from "react-router-dom";
 import * as db from "../Database";
-import { Card, Col, Row } from "react-bootstrap";
+import { Card, Col, FormControl, Row } from "react-bootstrap";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Dashboard() {
-    const courses = db.courses;
+    const [courses, setCourses] = useState<any[]>(db.courses);
+    const [course, setCourse] = useState<any>({
+        _id: "0", name: "New Course", number: "New Number",
+        startDate: "2023-09-10", endDate: "2023-12-15",
+        image: "/images/reactjs.jpg", description: "New Description"
+    });
+    const addNewCourse = () => {
+        const newCourse = { ...course, _id: uuidv4() };
+        setCourses([...courses, newCourse]);
+    };
+    const deleteCourse = (courseId: string) => {
+        setCourses(courses.filter((course) => course._id !== courseId));
+    };
+
+
     return (
         <div id="wd-dashboard" className="p-2">
             <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+            <h5>New Course
+                <button className="btn btn-primary float-end"
+                    id="wd-add-new-course-click"
+                    onClick={addNewCourse} > Add </button>
+            </h5><br />
+            <FormControl value={course.name} className="mb-2" onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+            <FormControl value={course.description} rows={3} onChange={(e) => setCourse({ ...course, description: e.target.value })} />
+            <hr />
             <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
             <div id="wd-dashboard-courses">
                 <Row xs={1} md={5} className="g-4">
@@ -39,6 +63,13 @@ export default function Dashboard() {
                                         <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
                                             {course.description} </p>
                                         <button className="btn btn-primary"> Go </button>
+                                        <button onClick={(event) => {
+                                            event.preventDefault();
+                                            deleteCourse(course._id);
+                                        }} className="btn btn-danger float-end"
+                                            id="wd-delete-course-click">
+                                            Delete
+                                        </button>
                                     </div>
                                 </Link>
                             </Card>
