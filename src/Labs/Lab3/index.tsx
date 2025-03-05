82733cc34a41c608e0276bf7af0a60a25fd9c8ae
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-03-04 19:07:03 -0500

    labs done - check rendering

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,3 +1,4 @@
+import { ListGroup } from "react-bootstrap";
 import Add from "./Add";
 import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
 import ArrowFunctions from "./ArrowFunctions";
@@ -30,51 +31,59 @@ import VariableTypes from "./VariableTypes";
 import VariablesAndConstants from "./VariablesAndConstants";
 import TodoItem from "./todos/TodoItem";
 import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
 
 export default function Lab3() {
   console.log('Hello World!');
-    return (
-      <div id="wd-lab3">
-        <h3>Lab 3</h3>
-        <VariablesAndConstants/>
-        <VariableTypes />
-        <BooleanVariables />
-        <IfElse />
-        <TernaryOperator />
-        <ConditionalOutputIfElse />
-        <ConditionalOutputInline />
-        <LegacyFunctions />
-        <ArrowFunctions />
-        <ImpliedReturn />
-        <TemplateLiterals />
-        <SimpleArrays />
-        <AddingAndRemovingToFromArrays />
-        <ForLoops />
-        <MapFunction />
-        <FindFunction />
-        <FindIndex />
-        <FilterFunction />
-        <JsonStringify />
-        <House />
-        <TodoItem />
-        <TodoList />
-        <Spreading />
-        <Destructing />
-        <FunctionDestructing />
-        <DestructingImports />
-        <Classes />
-        <Styles />
-        <Add a={3} b={4} />
-        <h4>Square of 4</h4>
-        <Square>4</Square>
-        <hr />
-        <Highlight>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
-          vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
-       </Highlight>
-       <PathParameters />
-       
-      </div>
-    );
-  }
-  
+  const { todos } = useSelector((state: any) => state.todosReducer);
+  return (
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <ListGroup>
+        {todos.map((todo: any) => (
+          <ListGroup.Item key={todo.id}>
+            {todo.title}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+      <hr />
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+      </Highlight>
+      <PathParameters />
+    </div>
+  );
+}
--- src/Labs/store/index.ts
+++ src/Labs/store/index.ts
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "../Lab4/ReduxExamples/HelloRedux/helloReducer";
+import counterReducer from "../Lab4/ReduxExamples/CounterRedux/counterReducer";
+import addReucer from "../Lab4/ReduxExamples/AddRedux/addReducer";
+import todosReducer from "../Lab4/ReduxExamples/todos/todosReducer";
+
 const store = configureStore({
-  reducer: { helloReducer }});
+  reducer: { helloReducer, counterReducer, addReucer, todosReducer },
+});
 export default store;
