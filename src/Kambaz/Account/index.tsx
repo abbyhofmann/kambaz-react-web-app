2c68f788471d5a1f557e0263290108a4cddd167e
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-03-09 22:40:02 -0400

    profile accounts done; need to do on your own

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -3,8 +3,11 @@ import Signin from "./Signin";
 import Profile from "./Profile";
 import Signup from "./Signup";
 import AccountNavigation from "./Navigation";
+import { useSelector } from "react-redux";
 
 export default function Account() {
+    const { currentUser } = useSelector((state: any) => state.accountReducer);
+
     return (
         <div id="wd-account-screen" className="p-2">
             <div className="d-flex">
@@ -14,7 +17,7 @@ export default function Account() {
                 <div className="">
 
                     <Routes>
-                        <Route path="/" element={<Navigate to="/Kambaz/Account/Signin" />} />
+                        <Route path="/" element={<Navigate to={currentUser ? "/Kambaz/Account/Profile" : "/Kambaz/Account/Signin"} />} />
                         <Route path="/Signin" element={<Signin />} />
                         <Route path="/Profile" element={<Profile />} />
                         <Route path="/Signup" element={<Signup />} />
