e44ccae09dc56e7d1c98c2fc4eb271a46b559aa3
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-03-25 09:58:26 -0400

    add module

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -3,26 +3,39 @@ import ModulesControls from "./ModulesControls";
 import LessonControlButtons from "./LessonControlButtons";
 import ModuleControlButtons from "./ModuleControlButtons";
 import { BsGripVertical } from "react-icons/bs";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FormControl } from "react-bootstrap";
-import { addModule, editModule, updateModule, deleteModule } from "./reducer";
+import { setModules, addModule, editModule, updateModule, deleteModule } from "./reducer";
 import { useSelector, useDispatch } from "react-redux";
+import * as coursesClient from "../client";
 
 export default function Modules() {
   const { cid } = useParams();
   const { modules } = useSelector((state: any) => state.modulesReducer);
   const dispatch = useDispatch();
+  const fetchModules = async () => {
+    const modules = await coursesClient.findModulesForCourse(cid as string);
+    dispatch(setModules(modules));
+  };
+  useEffect(() => {
+    fetchModules();
+  }, []);
+
   const [moduleName, setModuleName] = useState("");
+
+  const createModuleForCourse = async () => {
+    if (!cid) return;
+    const newModule = { name: moduleName, course: cid };
+    const module = await coursesClient.createModuleForCourse(cid, newModule);
+    dispatch(addModule(module));
+  };
 
   return (
     <div>
       <ModulesControls
         setModuleName={setModuleName}
         moduleName={moduleName}
-        addModule={() => {
-          dispatch(addModule({ name: moduleName, course: cid }));
-          setModuleName("");
-        }}
+        addModule={createModuleForCourse}
       />
       <br />
       <br />
@@ -30,7 +43,6 @@ export default function Modules() {
       <br />
       <ul id="wd-modules" className="list-group rounded-0 p-3">
         {modules
-          .filter((module: any) => module.course === cid)
           .map((module: any) => (
             <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
               <div className="wd-title p-3 ps-2 bg-secondary">
--- src/Kambaz/Courses/client.ts
+++ src/Kambaz/Courses/client.ts
@@ -2,6 +2,19 @@ import axios from "axios";
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 
+export const createModuleForCourse = async (courseId: string, module: any) => {
+  const response = await axios.post(
+    `${COURSES_API}/${courseId}/modules`,
+    module
+  );
+  return response.data;
+};
+
+export const findModulesForCourse = async (courseId: string) => {
+  const response = await axios.get(`${COURSES_API}/${courseId}/modules`);
+  return response.data;
+};
+
 export const fetchAllCourses = async () => {
   const { data } = await axios.get(COURSES_API);
   console.log("fetchall courses data: ", data);
