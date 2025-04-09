9531ff697a4d7c5b3c9bd58c0492c16f2fafbd2d
Abby Hofmann <abbyh@MacBook-Pro-4.local>
2025-04-09 12:26:56 -0400

    update course

--- src/Kambaz/Courses/client.ts
+++ src/Kambaz/Courses/client.ts
@@ -48,6 +48,6 @@ export const deleteCourse = async (id: string) => {
 };
 
 export const updateCourse = async (course: any) => {
-  const { data } = await axios.put(`${COURSES_API}/${course._id}`, course);
+  const { data } = await axiosWithCredentials.put(`${COURSES_API}/${course._id}`, course);
   return data;
 };
